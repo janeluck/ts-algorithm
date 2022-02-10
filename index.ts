7d3f7b9d6d43d8b6fcd363d912431d3498cdf3e3
janeluck <janeluck158@gmail.com>
2022-02-10 20:59:11 +0800

    reverse-integer

---
+++ index.ts
@@ -0,0 +1,27 @@
+/*
+* 整数反转
+链接：https://leetcode-cn.com/problems/reverse-integer
+* */
+function reverse(x: number): number {
+    const max = Math.pow(2, 31) - 1;
+    const maxLimit = Math.floor(max / 10);
+    let i = x, result = 0;
+    while (i != 0) {
+        if (result > maxLimit || result < 0 - maxLimit ) {
+            return 0;
+        }
+        if (result === maxLimit && (i % 10 > max % 10)) {
+            return 0;
+        }
+        if (result === 0 - maxLimit && (i % 10 < (0 - (max % 10 + 1)))) {
+            return 0;
+        }
+        result = result * 10 + i % 10;
+        if(x < 0){
+            i = 0 - Math.floor(Math.abs(i) / 10);
+        }else {
+            i = Math.floor(i / 10);
+        }
+    }
+    return result;
+};
