abff778fe750679063473691f73524dcc5a09dea
janeluck <janeluck158@gmail.com>
2022-02-11 12:39:33 +0800

    three sum

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-/*
-* 整数反转
-链接：https://leetcode-cn.com/problems/reverse-integer
-* */
-function reverse(x) {
-    var max = Math.pow(2, 31) - 1;
-    var maxLimit = Math.floor(max / 10);
-    var i = x, result = 0;
-    while (i != 0) {
-        if (result > maxLimit || result < 0 - maxLimit) {
-            return 0;
-        }
-        if (result === maxLimit && (i % 10 > max % 10)) {
-            return 0;
-        }
-        if (result === 0 - maxLimit && (i % 10 < (0 - (max % 10 + 1)))) {
-            return 0;
-        }
-        result = result * 10 + i % 10;
-        if (x < 0) {
-            i = 0 - Math.floor(Math.abs(i) / 10);
-        }
-        else {
-            i = Math.floor(i / 10);
+function threeSum(nums) {
+    var result = [];
+    if (nums.length < 3)
+        return result;
+    nums.sort(function (a, b) {
+        return a - b;
+    });
+    for (var i = 0; nums[i] <= 0; i++) {
+        if (i === 0 || nums[i] !== nums[i - 1]) {
+            for (var j = i + 1; j < nums.length; j++) {
+                if ((j === i + 1) || nums[j] !== nums[j - 1]) {
+                    for (var k = j + 1; k < nums.length; k++) {
+                        if ((k === j + 1) || nums[k] !== nums[k - 1]) {
+                            if (nums[i] + nums[j] + nums[k] === 0) {
+                                result.push([nums[i], nums[j], nums[k]]);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
     return result;
