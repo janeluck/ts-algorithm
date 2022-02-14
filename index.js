e483df9f0139423e4cb32a209958acb61112acea
janeajian <janeajian@tencent.com>
2022-02-14 13:35:13 +0800

    reverseArray

--- index.js
+++ index.js
@@ -3,5 +3,11 @@
  Do not return anything, modify nums in-place instead.
  */
 function rotate(nums, k) {
+    nums = nums.reverse();
+    const index = k % nums.length;
+    const first = nums.slice(0, index).reverse();
+    const second = nums.slice(index).reverse();
+    nums = first.concat(second);
+    console.log(nums);
 }
-;
+rotate([1, 2, 3, 4, 5, 6, 7], 3);
